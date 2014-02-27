0bc232127075ed268f0976f1d39bd0e46e8bdb56
David O Malley <davidmartinomalley@yahoo.com>
2014-02-27 00:21:25 -0500

    Various changes

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ function toggleMainImage(){
     if (document.getElementById('mainActionImage').getAttribute('src') == "checkMarkOff.png")
     {
         mainActionImage.src='checkMark.png';
+        giveFeedback();
     }
     else{
             mainActionImage.src='checkMarkOff.png';
+            removeFeedback();
     //alert('path2'&mainActionImage.src)
     };
     //alert('ended')
+}
+
+function giveFeedback(){
+    $('#feedback').html("Great work, keep it up!")
+}
+
+function removeFeedback(){
+    $('#feedback').html("")
 }
